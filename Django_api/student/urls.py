f37976b478990548441cba2dde066d5b252c62c3
496545717@qq.com <496545717@qq.com>
2017-08-30 07:02:35 +0800

    提交

--- Django_api/student/urls.py
+++ Django_api/student/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # “(?P<name>...) 子串匹配到的内容将可以用命名的name来提取url中的值。组的name必须是有效的python标识符，而且在本表达式内不重名。”
     url(r'studentdetail/(?P<name>.+)/$', StudentDetail.as_view()),
 
-    url(r'studentdetail/(?P<name>.+)/$', StudentDetail.as_view()),
+    url(r'studentdetail/$', StudentDetail.as_view()),
 ]
